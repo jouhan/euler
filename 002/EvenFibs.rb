a0dd0932779f6b855771bc8ae61f9e0d52a24ba7
Jouhan Allende <ednella@gmail.com>
2011-09-30 13:06:10 -0700

    moved even-checking outside fib function

--- 002/EvenFibs.rb
+++ 002/EvenFibs.rb
@@ -1,12 +1,12 @@
-# Returns numbers of Fib Sequence less than max
+# Returns numbers of Fibonacci Sequence < max
 def fib(max)
   a, b = 0, 1
   while b < max
     a,b = b,a+b
-    yield b if b.even?
+    yield b
   end
 end
 
 sum = 0
-fib(4_000_000) {|x| sum += x}
+fib(4_000_000) {|x| sum += x if x.even?}
 puts sum # => 4613732
