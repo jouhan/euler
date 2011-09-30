07aed8b06a27f22d55df6818d1a00e76582d3941
Jouhan Allende <ednella@gmail.com>
2011-09-29 22:44:56 -0700

    save fail. All organized now.

--- 001/NaturalNumbers.rb
+++ 001/NaturalNumbers.rb
@@ -1,7 +1,4 @@
-# Add all natural numbers < 1000 that are multiples of 3 or 5.
 # (Assumes valid input.)
-#
-# Returns sum.
 def calc(max, *mult)
   max.times.inject do |sum, i|
     sum += i if mult.any? {|m| i%m == 0}
--- NaturalNumbers.rb
+++
@@ -1,9 +0,0 @@
-# (Assumes valid input.)
-def calc(max, *mult)
-  max.times.inject do |sum, i|
-    sum += i if mult.any? {|m| i%m == 0}
-    sum
-  end
-end
-
-puts calc(1000, 3, 5) # => 233168
