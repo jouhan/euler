aa61098cab291df12a9e86432e6f1700b09e0f50
Jouhan Allende <ednella@gmail.com>
2011-10-03 11:23:19 -0700

    working state.

--- 003/Primes.rb
+++ 003/Primes.rb
@@ -4,25 +4,27 @@ def prime
   Enumerator.new { |y|
     primes, i = [2], 3
     loop do
-      y.yield i unless primes.any? { |p| i%p==0 }
+      unless primes.any? { |p| i%p==0 } then
+        primes << i
+        y.yield i
+      end
       i+=2
     end
   }
 end
 
+# (Assumes input with prime composites.)
 def factor(max)
-  p = 2
-  remaining = max
+  remaining, p = max, 2
+  enum = prime
 
   while remaining!=1 and p <= remaining
-    remaining%p==0 ? remaining/=p : p = prime.next
-    puts p
+    remaining%p==0 ? remaining/=p : p = enum.next
   end
+  p
 end
 
-
-puts prime.take(10)
-#factor(10)
+puts factor(600851475143)
 
 # Libraries. :)
 puts Prime.prime_division(600851475143).last.first
